c65b9afecfd0fb89b832d8b78c7814cdefdf8fe3
justine <justine.revenaz@gmail.com>
2018-07-25 18:20:47 -0500

    after download button

--- seed-time-sdk/src/App.js
+++ seed-time-sdk/src/App.js
@@ -7,6 +7,8 @@ import SdkMapReducer from '@boundlessgeo/sdk/reducers/map';
 import * as SdkMapActions from '@boundlessgeo/sdk/actions/map';
 // import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import { Well } from 'react-bootstrap';
+import { ButtonToolbar } from 'react-bootstrap';
 import { Jumbotron } from 'react-bootstrap';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import VeggieLine from './components/veggie-line';
@@ -20,16 +22,23 @@ import * as mapActions from '@boundlessgeo/sdk/actions/map';
 import * as drawingActions from '@boundlessgeo/sdk/actions/drawing';
 import {INTERACTIONS} from '@boundlessgeo/sdk/constants';
 import '@boundlessgeo/sdk/stylesheet/sdk.scss';
+import * as printActions from '@boundlessgeo/sdk/actions/print';
+import { saveAs } from 'file-saver';
+import SdkPrintReducer from '@boundlessgeo/sdk/reducers/print';
 
 
 
 
+
+// all the reducers used
 const store = createStore(combineReducers({
   'map': SdkMapReducer,
   'drawing': SdkDrawingReducer,
+  'print': SdkPrintReducer,
 }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+//all the constructors
 class App extends Component {
   constructor(props) {
     super(props);
@@ -37,6 +46,7 @@ class App extends Component {
     this.handleClickLi = this.handleClickLi.bind(this);
     this.handleClickPt = this.handleClickPt.bind(this);
     this.addFeature = this.addFeature.bind(this);
+    this.exportImage = this.exportImage.bind(this);
   }
   componentDidMount() {
     // add the OSM source
@@ -47,6 +57,7 @@ class App extends Component {
       source: 'osm',
       type: 'raster',
     }));
+
     // make stores for all three geometry types users can enter
     ['points', 'lines', 'polygons'].forEach((geom_type)=> {
       store.dispatch(mapActions.addSource(geom_type, {
@@ -82,7 +93,6 @@ class App extends Component {
       },
     }));
 
-
     // create the layer for lines
     store.dispatch(mapActions.addLayer({
       id: 'linez',
@@ -94,72 +104,84 @@ class App extends Component {
       },
     }));
 
-
-    // make the entries look good - later add veggie icons here
-
-
-
-
-
-}
-    addFeature(map, sourceName, collection) {
-      const feature = collection.features[0];
-      store.dispatch(mapActions.addFeatures(sourceName, [feature]));
-      console.log("wassup");
-    };
-
-    handleClickPoly() {
-      console.log("ahhhhhh");
-      store.dispatch(drawingActions.startDrawing('polygons', INTERACTIONS.polygon, 'direct_select'));
-    };
-    handleClickLi() {
-      console.log("ahhhhhh");
-      store.dispatch(drawingActions.startDrawing('lines', INTERACTIONS.line, 'direct_select'));
-    };
-     handleClickPt() {
-      console.log("ahhhhhh");
-      store.dispatch(drawingActions.startDrawing('points', INTERACTIONS.point, 'direct_select'));
-    };
-
-    render() {
-      return (
-        <div className="App">
-        <header className="App-header">
-
-        <h1 className="App-title">Seed Time</h1>
-        </header>
-        <SeedLogo />
-        <Jumbotron>
-        <h1>Welcome to Seed Time</h1>
-        <p>
-        Seed time helps you grow things.
-        </p>
-        </Jumbotron>
-        <Button bsStyle="custom">How it works</Button>
-        <p>  </p>
-        <div className="intro-list">
-        <ListGroup>
-        <ListGroupItem bsStyle="success">Choose a plot to grow on</ListGroupItem>
-        <ListGroupItem bsStyle="success">Choose what you would like to grow</ListGroupItem>
-        <ListGroupItem bsStyle="success">Get a customised map and calendar</ListGroupItem>
-        </ListGroup>
-        </div>
-        <VeggieLine />
-        <p>  </p>
-        <Button bsStyle="custom">Try out Seed Time!</Button>
-        <p>  </p>
-
-
-        <SdkMap store={store} onFeatureDrawn={this.addFeature}/>
-
-
-        <Button bsStyle="custom" active onClick={this.handleClickPoly}>Draw a polygon</Button>
-        <Button bsStyle="custom" active onClick={this.handleClickLi}>Draw a line</Button>
-        <Button bsStyle="custom" active onClick={this.handleClickPt}>Draw a point</Button>
-        </div>
-      );
-    }
   }
 
+  exportMapImage(blob) {
+    saveAs(blob, 'map.png');
+    store.dispatch(printActions.receiveMapImage());
+  };
 
-  export default App;
+  addFeature(map, sourceName, collection) {
+    const feature = collection.features[0];
+    store.dispatch(mapActions.addFeatures(sourceName, [feature]));
+  };
+
+  handleClickPoly() {
+    store.dispatch(drawingActions.startDrawing('polygons', INTERACTIONS.polygon, 'direct_select'));
+  };
+  handleClickLi() {
+    store.dispatch(drawingActions.startDrawing('lines', INTERACTIONS.line, 'direct_select'));
+  };
+  handleClickPt() {
+    store.dispatch(drawingActions.startDrawing('points', INTERACTIONS.point, 'direct_select'));
+  };
+
+  exportImage() {
+    store.dispatch(printActions.exportMapImage())
+  };
+
+
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+
+      <h1 className="App-title"></h1>
+      </header>
+      <SeedLogo />
+      <Jumbotron>
+      <h1>Welcome to Seed Time</h1>
+      <p>
+      Seed time helps you grow food.
+      </p>
+      </Jumbotron>
+      <Jumbotron>
+      <h2>How it works:</h2>
+      <p>
+      Choose a plot to grow on.
+      Choose what you would like to grow.
+      Get a customized plot and calendar.
+      </p>
+      </Jumbotron>
+      <p>  </p>
+      <VeggieLine />
+      <p>  </p>
+      <Button bsStyle="custom">Sup</Button>
+      <div> Try out Seed Time!</div>
+      <p>  </p>
+
+      <div className="main-map">
+      <SdkMap store={store} onFeatureDrawn={this.addFeature} onExportImage={this.exportMapImage} />
+      </div>
+
+      <ListGroupItem bsStyle="success">Find your plot on the map, draw it out, and start adding individual plants or rows of plants</ListGroupItem>
+
+      <div className="map-buttons">
+      <ButtonToolbar>
+      <Button bsStyle="custom" active onClick={this.handleClickPoly}>Draw out your plot</Button>
+      <Button bsStyle="custom" active onClick={this.handleClickLi}>Add a row of plants</Button>
+      <Button bsStyle="custom" active onClick={this.handleClickPt}>Add a plant</Button>
+      </ButtonToolbar>
+      <p> </p>
+      <Button bsStyle="custom" active>Done? Continue here</Button>
+      <p>  </p>
+      <Button bsStyle="custom" active onClick={this.exportImage}>Download your garden</Button>
+      </div>
+      </div>
+
+    );
+  }
+}
+
+
+export default App;
